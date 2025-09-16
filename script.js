be60c1e89d610392c577886d069307f2ad8867f8
Makvi-droid <pasbenauro05@gmail.com>
2025-09-16 09:11:17 +0800

    testing with dom functions

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-
-
-const number = 18;
+const num = document.getElementById('num');
+const btn = document.getElementById('btn');
 
 const generateSecretNumber = () => {
+    const guess = Number(num.value); // convert input to number
+    console.log(`Your guess is ${guess}`);
 
-    console.log(`Your number is  ${number}`)
+    const randomNum = Math.floor(Math.random() * 5) + 1; // change 5 -> 100 for full game
+    console.log(`Secret number is ${randomNum}`);
 
-   const randomNum = Math.floor(Math.random() * 100) + 1
-   console.log(randomNum)
-
-    if(number < randomNum){
-    console.log("your number is too low")
-    }
-    else if(number > randomNum){
-        console.log("your number is too high")
-    }
-    else if(number == randomNum){
-        console.log("you guessed the ryt number!")
+    if (guess < randomNum) {
+        console.log("Your number is too low");
+    } 
+    else if (guess > randomNum) {
+        console.log("Your number is too high");
+    } 
+    else {
+        console.log("You guessed the right number!");
     }
 
     return randomNum;
-}
+};
 
-generateSecretNumber()
-
-
-
-
-
+btn.addEventListener('click', generateSecretNumber);

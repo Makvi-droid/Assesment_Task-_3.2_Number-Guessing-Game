dbdaa8794a25024b15fe53135747e4f6d7a3b5b3
Makvi-droid <pasbenauro05@gmail.com>
2025-09-16 09:04:17 +0800

    testing conditions

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 
 
+const number = 18;
+
 const generateSecretNumber = () => {
-    return Math.floor(Math.random() * 100) + 1
+
+    console.log(`Your number is  ${number}`)
+
+   const randomNum = Math.floor(Math.random() * 100) + 1
+   console.log(randomNum)
+
+    if(number < randomNum){
+    console.log("your number is too low")
+    }
+    else if(number > randomNum){
+        console.log("your number is too high")
+    }
+    else if(number == randomNum){
+        console.log("you guessed the ryt number!")
+    }
+
+    return randomNum;
 }
+
+generateSecretNumber()
+
+
 
 
 
